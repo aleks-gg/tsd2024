b9f74218a5a5a9a840e8792f38aa4c0f5eb28cb4
Aleks Gostyński-Glina <49472609+aleks-gg@users.noreply.github.com>
2026-03-03 19:52:24 +0100

    Solve the Ameba task

--- 01-Introduction/Ameba.cs
+++ 01-Introduction/Ameba.cs
@@ -10,7 +10,12 @@ public class Ameba
 {
 	public int simulate(int[] X, int A)
 	{
-	    return default(int);
+		foreach (int g in X) {
+			if (g == A) {
+				A *= 2;
+			}
+		}
+		return A;
 	}
 
 	#region Testing code
