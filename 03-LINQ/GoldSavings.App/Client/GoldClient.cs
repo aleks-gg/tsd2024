c6fb4a7b7d82c0a7692db13bb202ace240d77f17
Aleks Gostyński-Glina <49472609+aleks-gg@users.noreply.github.com>
2026-03-17 17:43:19 +0000

    modify price retrieval to allow >93 day ranges

--- 03-LINQ/GoldSavings.App/Client/GoldClient.cs
+++ 03-LINQ/GoldSavings.App/Client/GoldClient.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using GoldSavings.App.Model;
+using System.ComponentModel.DataAnnotations;
 
 namespace GoldSavings.App.Client;
 
@@ -40,20 +41,23 @@ public class GoldClient
 
     public async Task<List<GoldPrice>> GetGoldPrices(DateTime startDate, DateTime endDate)
     {
-        string dateFormat = "yyyy-MM-dd";
-        string requestUri = $"cenyzlota/{startDate.ToString(dateFormat)}/{endDate.ToString(dateFormat)}";
-        HttpResponseMessage responseMsg = _client.GetAsync(requestUri).GetAwaiter().GetResult();
-        if (responseMsg.IsSuccessStatusCode)
+        List<GoldPrice> allPrices = new List<GoldPrice>();
+        for (int i = 0; i <= 90; i+=Math.Min(90, (endDate - startDate).Days))
         {
-            string content = await responseMsg.Content.ReadAsStringAsync();
-            List<GoldPrice> prices = JsonConvert.DeserializeObject<List<GoldPrice>>(content);
-            return prices;
-        }
-        else
-        {
-            return null;
-        }
+            string dateFormat = "yyyy-MM-dd";
+            string requestUri = $"cenyzlota/{startDate.AddDays(i).ToString(dateFormat)}/{endDate.AddDays(i).ToString(dateFormat)}";
+            HttpResponseMessage responseMsg = _client.GetAsync(requestUri).GetAwaiter().GetResult();
+            if (responseMsg.IsSuccessStatusCode)
+            {
+                string content = await responseMsg.Content.ReadAsStringAsync();
+                List<GoldPrice> prices = JsonConvert.DeserializeObject<List<GoldPrice>>(content);
+                if (prices != null)                {
+                    allPrices.AddRange(prices);
+                }
+            }
 
+        }
+        return allPrices;
     }
 
 }
