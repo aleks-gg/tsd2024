a45fd86fde5f9c5c1bdda259cf95117d7cb7f86f
Aleks Gostyński-Glina <49472609+aleks-gg@users.noreply.github.com>
2026-03-18 21:25:21 +0000

    add buy goldprice for step 2b

--- 03-LINQ/GoldSavings.App/Program.cs
+++ 03-LINQ/GoldSavings.App/Program.cs
@@ -11,40 +11,40 @@ class Program
         Console.WriteLine("Hello, Gold Investor!");
         
         GoldDataService dataService = new GoldDataService();
-        // Task 1
+        // Step 2a
         DateTime startDate = new DateTime(2025,01,01);
         DateTime endDate = new DateTime(2025,12,31);
         List<GoldPrice> goldPrices2025 = dataService.GetGoldPrices(startDate, endDate).GetAwaiter().GetResult();
         goldPrices2025 = goldPrices2025.OrderByDescending(x => x.Price).ToList();
-        Console.WriteLine("Task 1");
+        Console.WriteLine("Step 2a");
         Console.WriteLine($"Top 3 Gold prices in 2025: {string.Join(", ", goldPrices2025.Take(3).Select(x => x.Price))}");
         Console.WriteLine($"Bottom 3 Gold prices in 2025: {string.Join(", ", goldPrices2025.TakeLast(3).Select(x => x.Price))}");
         Console.WriteLine("---------------------------------------------");
         
-        // Task 2
+        // Step 2b
         List<GoldPrice> jan2020Prices = dataService.GetGoldPrices(new DateTime(2020,01,01), new DateTime(2020,01,31)).GetAwaiter().GetResult();
         GoldPrice buyPrice = jan2020Prices.OrderBy(x => x.Price).FirstOrDefault();
         List<GoldPrice> sellPrices = dataService.GetGoldPrices(buyPrice.Date, DateTime.Now).GetAwaiter().GetResult();
         sellPrices = sellPrices.Where(x => x.Price >= (buyPrice.Price * 1.05)).OrderByDescending(x => x.Price).ToList();
-        Console.WriteLine("\nTask 2\nTop 10 sell opportunities:");
+        Console.WriteLine($"\nStep 2b\nBuy on {buyPrice.Date} for {buyPrice.Price}, Top 10 sell opportunities:");
         foreach (var sellPrice in sellPrices.Take(10))
         {
             Console.WriteLine($"Sell on {sellPrice.Date} for {sellPrice.Price}");
         }
         Console.WriteLine("---------------------------------------------");
 
-        // Task 3
+        // Step 2c
         List<GoldPrice> prices2022_2019 = dataService.GetGoldPrices(new DateTime(2019,01,01), new DateTime(2022,12,31)).GetAwaiter().GetResult();
         prices2022_2019 = prices2022_2019.OrderByDescending(x => x.Price).ToList();
         List<GoldPrice> top3_in_second_10 = prices2022_2019.Skip(10).Take(3).ToList();
-        Console.WriteLine("\nTask 3\nTop 3 prices in 2019-2022 excluding top 10:");
+        Console.WriteLine("\nStep 2c\nTop 3 prices in 2019-2022 excluding top 10:");
         foreach (var price in top3_in_second_10)        {
             Console.WriteLine($"{price.Date}: {price.Price}");
         }
         Console.WriteLine("---------------------------------------------");
 
-        // Task 4
-        Console.WriteLine("\nTask 4\nAverage gold price in 2024:");
+        // Step 2d
+        Console.WriteLine("\nStep 2d\nAverage gold price in 2024:");
         List<GoldPrice> prices2020 = dataService.GetGoldPrices(new DateTime(2020,01,01), new DateTime(2020,12,31)).GetAwaiter().GetResult();
         var avgPrice2020 = prices2020.Average(x => x.Price);
         Console.WriteLine($"Average gold price in 2020: {avgPrice2020}");
@@ -58,8 +58,8 @@ class Program
         Console.WriteLine($"Average gold price in 2024: {avgPrice2024}");
         Console.WriteLine("---------------------------------------------");
 
-        // Task 5
-        Console.WriteLine("\nTask 5\nBest Deal 2020-2024:");
+        // Step 2e
+        Console.WriteLine("\nStep 2e\nBest Deal 2020-2024:");
         List<GoldPrice> prices2020_2024 = dataService.GetGoldPrices(new DateTime(2020,01,01), new DateTime(2024,12,31)).GetAwaiter().GetResult();
         var bestDeal = prices2020_2024
             .Select(buy => new {
@@ -78,6 +78,7 @@ class Program
         string filePath = "gold_prices_2020_2024.xml";
         xmlIO.SaveGoldPricesToXML(prices2020_2024, filePath);
 
+        // Step 4
         List<GoldPrice> loaded_prices = xmlIO.LoadGoldPricesFromXML(filePath);
         Console.WriteLine($"Are equal: {loaded_prices.SequenceEqual(prices2020_2024)}");
 
